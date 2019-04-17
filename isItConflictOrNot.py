f58db9ecc1eba1c7169cbe3a5b0c95a2034c491f
Konstantinova Vera <kamikofuroraru@gmail.com>
2019-04-17 23:54:45 +0300

    cleaning cache

--- isItConflictOrNot.py
+++ isItConflictOrNot.py
@@ -26,53 +26,55 @@ def isItConflictOrNot(ui, repo, source=None):
     cache_list = os.path.join(CACHE, 'cache_list')
 
     if not os.path.exists(cache_list):
-        f = open(cache_list, 'a')
-        f.close()
+        with open(cache_list, 'a') as f: pass
 
-    try:
-        if hg.islocal(clone_source):
-            commands.clone(repo.ui, clone_source, remote_clone_dir)
-
-        else:
-            f = open(cache_list, 'a+')
-            cache_source = None
-            cache_source_repo = None
-            for line in f:
+    if hg.islocal(clone_source):
+        commands.clone(repo.ui, clone_source, remote_clone_dir)
+    else:
+        cache_source = None
+        cache_source_repo = None
+        with open(cache_list, 'r') as f:
+            lines = f.readlines()
+            f.seek(0)
+            for line in lines:
                 if line.startswith(cur_dir + clone_source):
                     cache_source = line[:-1].replace(cur_dir + clone_source, '')
-                    cache_source_repo = os.path.join(cache_source, '.hg') # == False -> need to remove from list WANT OPTION TO CLEAN THISSSSS ALLLLLLLLLLLLLLL
-                    break
-            if (cache_source is None) or (not os.path.exists(cache_source_repo)):
-                cache_source = str(raw_input('Specify the path for the cache-repository.\n')).replace('\r', '')
-                try:
-                    os.makedirs(cache_source)
-                    f.write(cur_dir + clone_source + cache_source + '\n')
-                    f.close()
-                    commands.clone(repo.ui, clone_source, cache_source)
-                except OSError as e:
-                    if e.errno != errno.EEXIST:
-                        raise
-            else:
-                repo = hg.repository(repo.ui, cache_source)
-                if commands.incoming(repo.ui, repo, bundle=None, force=False) == 0:
-                    commands.pull(repo.ui, repo, clone_source)
-                    commands.update(repo.ui, repo)
-                repo = hg.repository(repo.ui, cur_dir)
+                    cache_source_repo = os.path.join(cache_source,
+                                                     '.hg')
 
-            commands.clone(repo.ui, cache_source, remote_clone_dir)
+        if (cache_source_repo is not None) and (not os.path.exists(cache_source_repo)):
+            with open(cache_list, 'r+') as f:
+                lines = f.readlines()
+                f.seek(0)
+                for line in lines:
+                    if line != (str(cur_dir + clone_source + cache_source) + '\n'):
+                        f.write(line)
+                f.truncate()
+            cache_source = None
 
-    except Exception as e:
-        traceback.print_exc(e)
-        return 0
+        if cache_source is None:
+            cache_source = str(raw_input('Specify the path for the cache-repository.\n')).replace('\r', '')
+            with open(cache_list, 'a') as f:
+                f.write(cur_dir + clone_source + cache_source + '\n')
+            os.makedirs(cache_source)
+            commands.clone(repo.ui, clone_source, cache_source)
+        else:
+            repo = hg.repository(repo.ui, cache_source)
+            if commands.incoming(repo.ui, repo, bundle=None, force=False) == 0:
+                commands.pull(repo.ui, repo, clone_source)
+                commands.update(repo.ui, repo)
+            repo = hg.repository(repo.ui, cur_dir)
+
+        commands.clone(repo.ui, cache_source, remote_clone_dir)
 
     commands.clone(repo.ui, cur_dir, local_clone_dir)
+
     repo = hg.repository(repo.ui, remote_clone_dir)
     commands.pull(repo.ui, repo, local_clone_dir)
+    commands.update(repo.ui, repo)
+    conflict_or_not = commands.merge(repo.ui, repo)
 
-    try:
-        commands.update(repo.ui, repo)
-        conflict_or_not = commands.merge(repo.ui, repo)
-
+    if conflict_or_not is True:
         repo.ui.pushbuffer()
         commands.resolve(repo.ui, repo, list=True)
         u_files_str = repo.ui.popbuffer()
@@ -81,17 +83,12 @@ def isItConflictOrNot(ui, repo, source=None):
         u_files_list = list(filter(None, u_files_list))
 
         for uFile in u_files_list:
-            f = open(os.path.join(remote_clone_dir, uFile), 'r')
             repo.ui.write('\n' + uFile + '\n')
-            repo.ui.write(f.read())
-            f.close()
+            with open(os.path.join(remote_clone_dir, uFile), 'r') as f:
+                repo.ui.write(f.read())
 
-    except Exception as e:
-        traceback.print_exc(e)
-        conflict_or_not = False
-
-    if conflict_or_not is True:
         repo.ui.write('\nYes, here is a conflict\n')
+
     else:
         repo.ui.write('\nNo, everything cool\n')
 
